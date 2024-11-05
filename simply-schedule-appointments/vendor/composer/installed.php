dd8d464b3a3f9d5f0717f90d28d4adb7606984cd
Sean Murphy <sean@iamseanmurphy.com>
2024-11-05 22:28:46 +0000

    1.6.7.63

--- simply-schedule-appointments/vendor/composer/installed.php
+++ simply-schedule-appointments/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '859b4454d40fdf3efb9229c473447479fbd1cd16',
+        'reference' => '14ec230197fa8551bbe967ac61be2899108be53c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '859b4454d40fdf3efb9229c473447479fbd1cd16',
+            'reference' => '14ec230197fa8551bbe967ac61be2899108be53c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
